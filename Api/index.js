c514622f07f1a4c0512fd0dea06897111c659521
Senadhian2003 <senadhian@gmail.com>
2024-03-19 19:42:43 +0530

    first commit sena

--- Api/index.js
+++ Api/index.js
@@ -7,20 +7,12 @@ app.use(CORS());
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended : true}));
 const { Configuration, OpenAIApi } = require("openai");
-
-
-
 const openAi = new OpenAIApi(
   new Configuration({
     apiKey: process.env.OPEN_AI_API_KEY,
   })
 )
-// app.post("/check",async(req,res)=>{
 
-//   const { input } = req.body;
-//   console.log(input)
-//   res.json("ko")
-// })
 app.get("/",async (req,res)=>{
   res.send("welcome")
 
@@ -28,7 +20,7 @@ app.get("/",async (req,res)=>{
 app.post("/genai", async(req,res)=>{
   
   const { input } = req.body;
-  console.log(input)
+  // console.log(input)
   try{
     const response = await openAi.createChatCompletion({
       model: "gpt-3.5-turbo",
@@ -44,22 +36,7 @@ app.post("/genai", async(req,res)=>{
   // res.send("jo")
 })
 
-// const userInterface = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// })
-
-// userInterface.prompt()
-// userInterface.on("line", async input => {
-//   const response = await openAi.createChatCompletion({
-//     model: "gpt-3.5-turbo",
-
-//     messages: [{ role: "user", content: input }],
-//   })
-//   console.log(response.data.choices[0].message.content)
-//   userInterface.prompt()
-// })
 
 app.listen(5000,()=>{
-  console.log("Server running in 5000")
+  console.log("Server running in 5000")
 })
